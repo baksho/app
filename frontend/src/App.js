5e9fec91dbb573e2a49800746c3e69c19aa168df
Suvinava Basak <ultimate.basak@gmail.com>
2026-02-13 13:18:32 +0000

    auto-commit for 32f9cc60-80c5-4176-865e-5616d60edf95

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Experience from "./components/Experience";
+import Education from "./components/Education";
 import Projects from "./components/Projects";
 import Blogs from "./components/Blogs";
 import Contact from "./components/Contact";
